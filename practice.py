31928c3fc84d14316870760393e9ca679a7bf459
vg634655-prog <vg634655@gmail.com>
2026-07-10 08:59:12 +0530

    practice.py

---
+++ practice.py
@@ -0,0 +1,11 @@
+ep1 = {"name":"vikas","age":"25","pay":"25000"}
+
+ep2 = {"work":"12 hours","time":"9:7"}
+
+#ep1.update(ep2) #help to update a output
+#ep1.pop("name") #help to remove any item from the list
+#ep1.clear() #help to remove all things from the bracket
+#ep1.popitem() #help to remove last item from the bracket
+#del ep1 #is not defind any things
+print(ep1)
+
